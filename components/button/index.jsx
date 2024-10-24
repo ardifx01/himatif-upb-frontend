fb489e76705bd29d0dcf021ae4f19c3d3b3f2355
Alipurnama <Alipurnama69@gmail.com>
2024-10-24 16:29:59 +0700

    [Add] profile button

--- components/button/index.jsx
+++ components/button/index.jsx
@@ -1,7 +1,7 @@
 function Button({ text, onClick }) {
   return (
     <button
-      className="bg-gradient-to-r from-0 to-5 text-white p-2 shadow-xl hover:shadow-2xl w-full text-xl rounded-setup hover:bg-gradient-to-l transition-all duration-1000 ease-in-out"
+      className="bg-gradient-to-r from-0 to-5 text-white p-2 shadow-xl hover:shadow-2xl w-full text-xl rounded-setup hover:bg-gradient-to-l border-[2px] border-white"
       onClick={onClick}
     >
       {text}
