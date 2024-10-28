299eacdb9d0b77db0b9c330e8362e080ad596a77
Alipurnama <Alipurnama69@gmail.com>
2024-10-28 16:20:12 +0700

    [Fix] Favicon & Action Button Profil

--- pages/profile/action/index.jsx
+++ pages/profile/action/index.jsx
@@ -2,7 +2,7 @@ import Button from "@/components/button";
 
 const ActionSection = () => (
   <div className="actionbutton flex-col gap-5 max-md:w-full flex w-1/3">
-    <Button text="Our Location" link="/profile" />
+    <Button text="Profile & History" link="/profile" />
     <Button text="Our Location" link="/profile/location" />
   </div>
 );
